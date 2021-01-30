aa2869c6b1d5ceaf3145904f529c9cfd5c360f2b
Daniel YU <i@icymint.me>
2021-01-30 12:12:26 +0800

    fix subnets

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ locals {
       node           = node
       link = { for l in local.links[name] : l => {
         pubkey    = local.servers[l].key.pub
-        subnets   = flatten([[format("%s/32", local.servers[l].ip)], coalesce(node.con[l]) ? [node.con[l].replace ? [] : local.servers[l].sub, node.con[l].subnets] : local.servers[l].sub])
+        subnets   = flatten([[format("%s/32", local.servers[l].ip)], can(node.con[l]) ? [node.con[l].replace ? [] : local.servers[l].sub, node.con[l].subnets] : [local.servers[l].sub]])
         keepalive = can(node.con[l]) ? node.con[l].keepalive : local.defaultPersistentKeepalive
       } }
     })
